6a260826a62a277b02df1b4f9e569eed37448e7a
silent killer <75825050+hy7h5@users.noreply.github.com>
2022-02-25 15:12:48 +0600

    This Tool upload via Silent Killer
    This is a Calculator Tool.This tool is maked By Antick Roy. This tool is only use for educational purposes.

---
+++ Calculator.py
@@ -0,0 +1,67 @@
+black="\033[0;30m"        # Black
+red="\033[0;31m"          # Red
+green="\033[0;32m"        # Green
+yellow="\033[0;33m"       # Yellow
+blue="\033[0;34m"         # Blue
+purple="\033[0;35m"       # Purple
+cyan="\033[0;36m"         # Cyan
+white="\033[0;37m"        # White
+
+print (red+ "Welcome To Silent  Killer")
+
+# Program make a simple calculator
+import os 
+  
+# This function adds two numbers
+
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print(purple+"1.Add")
+print(yellow+"2.Subtract")
+print(green+"3.Multiply")
+print(cyan+"4.Divide")
+
+while True:
+    # take input from the user
+    choice = input(red+"Enter choice(1/2/3/4): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input(yellow+"Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    
+    else:
+        print("Invalid Input")
